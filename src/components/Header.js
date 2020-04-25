eaea39653ce4d3f0a16783a338ffabb9d9e6d8e0
Gustavo Modena <gustavo@follow55.com.br>
2020-04-25 02:51:52 -0300

    chore(header): create header desktop structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
   return (
     <header className="header">
       <div className="header-row row">
-        <p>header</p>
+        <img className="logo" src={require('../images/logo.webp')} alt=""/>
+
+        <nav className="nav">
+					<ul className="nav-list">
+						<li className="nav-item">
+							<NavLink className="nav-link" to="/">Home</NavLink>
+						</li>
+						
+						<li className="nav-item">
+							<NavLink className="nav-link" to="/about">Sobre</NavLink>
+						</li>
+
+						<li className="nav-item">
+							<NavLink className="nav-link" to="#">Jogos</NavLink>
+						</li>
+
+						<li className="nav-item">
+							<NavLink className="nav-link" to="#">Assassinos</NavLink>
+						</li>
+
+						<li className="nav-item">
+							<NavLink className="nav-link" to="#">Notícias</NavLink>
+						</li>
+					</ul>
+        </nav>
       </div>
     </header>
   );
