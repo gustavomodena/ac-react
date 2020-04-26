ccc301eed533d6ec2eae59efa1f139dd82b92d46
Gustavo Modena <gustavo@follow55.com.br>
2020-04-26 13:23:56 -0300

    chore(Button): update Button component style

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Button = ({text, to})=> {
-	return (
-		<Link className="button button-primary" to={to}>{text}</Link>
-	);
-}
+const Button = ({text, to})=> (
+	<Link className="button button-primary" to={to}>{text}</Link>
+);
 
 export default Button;
